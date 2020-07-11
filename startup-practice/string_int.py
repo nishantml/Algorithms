9e2377c33dcb38d94d93b579f13d768d62417cb5
nishant <nishantbhushan10@gmail.com>
2020-07-11 23:06:22 +0530

    Create a function that takes a string and returns it as an integer.

---
+++ startup-practice/string_int.py
@@ -0,0 +1,23 @@
+"""
+
+Create a function that takes a string and returns it as an integer.
+
+Examples
+string_int("6") ➞ 6
+
+string_int("1000") ➞ 1000
+
+string_int("12") ➞ 12
+Notes
+All numbers will be whole numbers.
+
+"""
+
+
+def string_int(txt):
+    return int(txt)
+
+
+print(string_int("6"))
+print(string_int("1000"))
+print(string_int("12"))
