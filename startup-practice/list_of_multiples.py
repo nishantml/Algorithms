3648787f1eedd0875fbddddb2e68c19afeeeec52
nishant <nishantbhushan10@gmail.com>
2020-07-12 01:42:17 +0530

    list_of_multiples

---
+++ startup-practice/list_of_multiples.py
@@ -0,0 +1,21 @@
+"""
+
+Create a function that takes two numbers as arguments (num, length) and returns a list of multiples of num up to length.
+
+Examples
+list_of_multiples(7, 5) ➞ [7, 14, 21, 28, 35]
+
+list_of_multiples(12, 10) ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
+
+list_of_multiples(17, 6) ➞ [17, 34, 51, 68, 85, 102]
+Notes
+Notice that num is also included in the returned list.
+
+"""
+
+
+def list_of_multiples(num, length):
+    lst = []
+    for i in range(1, length + 1):
+        lst.append(num * i)
+    return lst
