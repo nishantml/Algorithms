2eae38e498403cd1d1ed33f5b511fab1c4403e28
Nishant <nishantbhushan10@gmail.com>
2020-07-24 23:15:01 +0530

    get_indices

---
+++ startup-practice/get_indices.py
@@ -0,0 +1,26 @@
+"""
+
+Create a function that returns the indices of all occurrences of an item in the list.
+
+Examples
+get_indices(["a", "a", "b", "a", "b", "a"], "a") ➞ [0, 1, 3, 5]
+
+get_indices([1, 5, 5, 2, 7], 7) ➞ [4]
+
+get_indices([1, 5, 5, 2, 7], 5) ➞ [1, 2]
+
+get_indices([1, 5, 5, 2, 7], 8) ➞ []
+Notes
+If an element does not exist in a list, return [].
+Lists are zero-indexed.
+Values in the list will be value-types (don't need to worry about nested lists).
+
+"""
+
+
+def get_indices(lst, el):
+    ind = []
+    for i in range(len(lst)):
+        if el == lst[i]:
+            ind.append(i)
+    return ind
