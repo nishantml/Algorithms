2f604ef8c9db60d6e7ef0a621e4c8d7f5d47b9ab
nishant <nishantbhushan10@gmail.com>
2020-08-09 22:57:58 +0530

    stack using array

---
+++ basics-data-structure/stack/stack-using-array.py
@@ -0,0 +1,47 @@
+class Stack:
+    def __init__(self):
+        self.array = []
+        self.length = 0
+
+    def push(self, data):
+        self.array.append(data)
+        self.length += 1
+        return
+
+    def pop(self):
+        poped = self.array[-1]
+        self.array.pop(self.length - 1)
+        self.length -= 1
+        return poped
+
+    def lookup(self):
+        for val in reversed(self.array):
+            print('-', val)
+
+    def peek(self):
+        return self.array[-1]
+
+    def reverse(self):
+        for i in range(self.length - 1):
+            temp = self.array[i]
+            self.array[i] = self.array[i + 1];
+            self.array[i + 1] = temp
+
+        self.lookup()
+
+
+myStack = Stack()
+print('Length of node is ->', myStack.length)
+myStack.push('google')
+myStack.push('gmail')
+myStack.push('check')
+myStack.push('facebook')
+myStack.lookup()
+print('Length of node is ->', myStack.length)
+print('Top node is ->', myStack.peek())
+print('Poped node is ->', myStack.pop())
+myStack.lookup()
+print('Length of node is ->', myStack.length)
+print('\nStacked reversed ')
+myStack.reverse()
+print('\nTop node is ->', myStack.peek())
