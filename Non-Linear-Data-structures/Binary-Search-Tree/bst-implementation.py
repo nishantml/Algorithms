ab5b3b0359d500cba58403f8bc273015aadc8be0
Nishant <nishantbhushan10@gmail.com>
2020-09-02 22:57:39 +0530

    bst -> find min and max value

--- Non-Linear-Data-structures/Binary-Search-Tree/bst-implementation.py
+++ Non-Linear-Data-structures/Binary-Search-Tree/bst-implementation.py
@@ -49,7 +49,20 @@ class BST:
 
         return False
 
-    # inoder traversal of the BST
+    def minValueNode(self):
+        curr_node = self.root
+        while curr_node.left is not None:
+            curr_node = curr_node.left
+
+        return curr_node.val
+
+    def maxValueNode(self):
+        curr_node = self.root
+        while curr_node.right is not None:
+            curr_node = curr_node.right
+
+        return curr_node.val
+
     def inorder(self, root):
         currentNode = root
         if root:
@@ -59,13 +72,16 @@ class BST:
 
 
 bst = BST()
-bst.insert(1)
+# bst.insert(1)
 bst.insert(3)
 bst.insert(10)
 bst.insert(4)
 bst.insert(5)
 bst.insert(6)
-print('Search {10} in bst', bst.lookup(10))
+# print('Search {10} in bst', bst.lookup(10))
 print('Search {21} in bst', bst.lookup(21))
 print('\nInorder traverse of BST')
 bst.inorder(bst.root)
+
+print('min value in bst -> ', bst.minValueNode())
+print('max value in bst -> ', bst.maxValueNode())
