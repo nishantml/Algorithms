c2b9a7ef40cfe4aa21f218b75a78ae175230dc8b
nishantml <nishantbhushan10@gmail.com>
2021-02-15 23:43:12 +0530

    Rotate Array

--- complete-dsa/array/rotate-array-by-one.py
+++ complete-dsa/array/rotate-array-by-one.py
@@ -10,19 +10,21 @@ def rotate(arr, n):
     if len(arr) < n:
         print('Value of n should ne less than or equal to array size')
         return
-    right_shift = arr[:n]
-    left_shift = arr[n:]
+    right_shift = arr[:len(arr) - n]
+    left_shift = arr[len(arr) - n:]
+    # print(right_shift)
+    # print(left_shift)
 
-    right_shift_start = len(arr) - n
-    for i in range(right_shift_start):
+    for i in range(n):
+        # print(i)
         arr[i] = left_shift[i]
 
     count = 0
-    for j in range(right_shift_start, len(arr)):
+    for j in range(n, len(arr)):
         arr[j] = right_shift[count]
         count += 1
 
     print(arr)
 
 
-rotate([1, 2, 3, 4, 5], 4)
+rotate([1, 2, 3, 4, 5, 6], 2)
