c4d0db4dee1d1defec1809fb222aea7443681142
nishantml <nishantbhushan10@gmail.com>
2021-02-18 23:36:06 +0530

    kthSmallest intersection

---
+++ leet/intersection.py
@@ -0,0 +1,31 @@
+"""
+349. Intersection of Two Arrays
+
+Given two arrays, write a function to compute their intersection.
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Note:
+
+Each element in the result must be unique.
+The result can be in any order.
+
+"""
+from typing import List
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        inter = []
+        for num in nums1:
+            if num in nums2:
+                if num not in inter:
+                    inter.append(num)
+
+        return inter
